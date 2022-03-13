044d4e136460ecc6817fa9bd56057ef7597c0237
Andrew Claasen <andrew@mrclaasen.com>
2022-03-12 23:51:31 -0800

    add background

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 </script>
 
 <template>
-    <h1 class="padding bg-color1" style="margin:0;">
+    <h1 class="padding bg-color1 z1" style="margin:0;">
         Insta-Mine for No Man's Sky
     </h1>
 
-    <mine-setup></mine-setup>
+    <div class="container">
+        <mine-setup></mine-setup>
+    </div>
 </template>
