291dcaab690e4e3c9620dd0305931e1d0e337f7a
Andrew Claasen <andrew@mrclaasen.com>
2022-03-19 04:03:18 -0700

    fix rounding error on timestamps

--- src/js/NmsBase.js
+++ src/js/NmsBase.js
@@ -90,7 +90,7 @@ class NmsBase{
     }
 
     updateTimestamps(){
-        let timestamp = (new Date() / 1000) - (60*60*24*7)
+        let timestamp = Math.round(new Date() / 1000) - (60*60*24*7)
         //let timestamp = Math.round((new Date() / 1000) - (60*60))
         //let timestamp = Math.round(new Date() / 1000)
     
